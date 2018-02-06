0208bd457e1f17636cff51e9778eec6f16dc5dca
Austen Lamacraft <austen@lamacraft.io>
2018-02-06 23:45:20 +0000

    sporifica virus part two

--- sporifica_virus.py
+++ sporifica_virus.py
@@ -7,7 +7,7 @@ with open("inputs/sporifica_virus.txt") as file:
         initial.append(list(line[:-1]))
 
 
-dirs = {0: np.array([-1,0]), 1: np.array([0,1]), 2: np.array([1,0]), 3: np.array([0,-1])}
+dirs = [np.array([-1,0]), np.array([0,1]), np.array([1,0]), np.array([0,-1])]
 
 facing = 0
 
@@ -20,21 +20,29 @@ grid = np.zeros((max_size, max_size), dtype = int)
 for row, _ in enumerate(initial):
     for col, _ in enumerate(initial[row]):
         if initial[row][col] == "#":
-            grid[offset + row, offset + col] = 1
+            grid[offset + row, offset + col] = 2
 
-steps = 10000
+steps = 10000000
 count = 0
 
-for _ in range(steps):
-    if grid[tuple(pos)] == 1:
-        facing = (facing + 1) % 4
-        grid[tuple(pos)] = 0
-    else:
+# Use encoding 0: clean, 1: weakened, 2: infected, 3: flagged
+
+for j in range(steps):
+    if grid[tuple(pos)] == 0:
         facing = (facing - 1) % 4
-        grid[tuple(pos)] = 1
+    elif grid[tuple(pos)] == 2:
+        facing = (facing + 1) % 4
+    elif grid[tuple(pos)] == 3:
+        facing = (facing + 2) % 4
+
+    grid[tuple(pos)] = (grid[tuple(pos)] + 1)%4
+    if grid[tuple(pos)] == 2:
         count += 1
 
     pos += dirs[facing]
+
+    if j % 100000 == 0:
+        print(f"step {j}")
 
 print(count)
 
